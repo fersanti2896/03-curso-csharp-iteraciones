bf1af369c6b2327008dcc9974558c91e85a4fd09
Fernando Nicolás <fersanti2896@gmail.com>
2022-09-26 13:04:52 -0500

    Operador ternario en 04-operador-ternario.cs

---
+++ Codigo/04-operador-ternario.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecisionesAndBucles.Codigo {
+    public class _04_operador_ternario {
+        public void operadorTernario() {
+            bool usuarioLogueado = false;
+
+            string mensaje = usuarioLogueado ? "¡Bienvenido al Sistema!" : "¡Inicia Sesión para comenzar!";
+            Console.WriteLine(mensaje);
+        }  
+    }
+}
