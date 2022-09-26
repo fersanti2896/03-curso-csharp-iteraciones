78e05761fb02c2fea10bf1985e30df444113d47d
Fernando Nicolás <fersanti2896@gmail.com>
2022-09-26 12:37:57 -0500

    Condicional if en 03-condicional-if.cs

---
+++ Codigo/03-condicional-if.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecisionesAndBucles.Codigo {
+    public class _03_condicional_if {
+        public void condicionalIf()
+        {
+            var edad = 17;
+            Console.WriteLine($"Tu edad es { edad }");
+
+            if (edad >= 18) {
+                Console.WriteLine("¡Es mayor de edad!");
+            }
+            else {
+                Console.WriteLine("¡No es mayor de edad!");
+            }
+        }
+    }
+}
