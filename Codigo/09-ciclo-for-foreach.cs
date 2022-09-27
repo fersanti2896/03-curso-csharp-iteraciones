9644e75dd35447785750613fa602b4efa091d397
Fernando Nicolás <fersanti2896@gmail.com>
2022-09-27 00:09:22 -0500

    Ciclo For y Foreach en 09-ciclo-for-foreach.cs

---
+++ Codigo/09-ciclo-for-foreach.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecisionesAndBucles.Codigo {
+    public class _09_ciclo_for_foreach {
+        public void cicloFor() {
+            for (int i = 1; i <= 10; i++) {
+                Console.WriteLine($"Valor: { i }");
+            }
+
+            for (int i = 1; i <= 10; i++) {
+                Console.WriteLine($"Bucle For");
+            }
+        }
+
+        public void cicloForEach() {
+            string nombre = "Fer Santi";
+
+            foreach (var letra in nombre) {
+                Console.WriteLine($"Caracter: { letra }");
+            }
+        }  
+    }
+}
