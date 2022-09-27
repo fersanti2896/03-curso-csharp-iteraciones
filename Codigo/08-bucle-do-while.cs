4215c98b75e00d885cd647f13f304ed1dc320eb7
Fernando Nicolás <fersanti2896@gmail.com>
2022-09-27 00:00:55 -0500

    Ciclo Do While en 08-bucle-do-while.cs

---
+++ Codigo/08-bucle-do-while.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecisionesAndBucles.Codigo {
+    public class _08_bucle_do_while {
+        public void bucleDoWhile() {
+            var contador = 10;
+
+            do {
+                Console.WriteLine($"Valor: { contador }");
+            } while (contador > 10);
+        }
+    }
+}
