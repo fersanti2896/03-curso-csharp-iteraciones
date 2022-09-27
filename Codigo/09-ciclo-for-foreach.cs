73263bd1da182360818c06eb33f9937a97ce516d
Fernando Nicolás <fersanti2896@gmail.com>
2022-09-27 00:36:30 -0500

    Ciclo For y Foreach en 09-ciclo-for-foreach.cs

--- Codigo/09-ciclo-for-foreach.cs
+++ Codigo/09-ciclo-for-foreach.cs
@@ -14,6 +14,18 @@ namespace DecisionesAndBucles.Codigo {
             for (int i = 1; i <= 10; i++) {
                 Console.WriteLine($"Bucle For");
             }
+
+            for (int i = 1; i <= 15; i++) {
+                if (i % 15 == 0) {
+                    Console.WriteLine("fizzbuzz");
+                } else if (i % 3 == 0) {
+                    Console.WriteLine("fizz");
+                } else if (i % 5 == 0) {
+                    Console.WriteLine("buzz");
+                } else {
+                    Console.WriteLine($"{ i } ");
+                }
+            }
         }
 
         public void cicloForEach() {
