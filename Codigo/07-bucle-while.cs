e82340600496fd681e56e3e356a17ee625f7192d
Fernando Nicolás <fersanti2896@gmail.com>
2022-09-26 23:53:24 -0500

    Ciclo While en 07-bucle-while.cs

---
+++ Codigo/07-bucle-while.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecisionesAndBucles.Codigo {
+    public class _07_bucle_while {
+        public void bucleWhile() {
+            var cantidad = 1;
+
+            while (cantidad <= 10) {
+                Console.WriteLine($"Valor: { cantidad }");
+                cantidad++;
+            }
+
+            var valor = 1;
+
+            while (valor <= 10) {
+                Console.WriteLine($"{ valor } x { 1 } = { valor * 1 }");
+                valor++;
+            }
+        }    
+    }
+}
