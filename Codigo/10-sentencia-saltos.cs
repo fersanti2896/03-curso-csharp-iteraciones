f233adb40b666bfdb9c8660d0d2a8142dfedbbcf
Fernando Nicolás <fersanti2896@gmail.com>
2022-10-03 02:14:41 -0500

    Sentencias en 10-sentencias-saltos.cs

---
+++ Codigo/10-sentencia-saltos.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecisionesAndBucles.Codigo {
+    public class _10_sentencia_saltos {
+        public void sentencia_saltos() {
+            for (int i = 1; i <= 10; i++) {
+                if (i == 5)
+                    break;
+
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("¡Fin del primer For!");
+
+            for (int i = 1; i <= 10; i++) {
+                if (i == 5)
+                    continue;
+
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("¡Fin del segundo For!");
+
+            Console.WriteLine("¡Fin del programa!");
+        }
+    }
+}
